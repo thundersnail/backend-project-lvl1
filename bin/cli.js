8374c4d71edd1c3f079202adc0a0811ab79e84cf
Yury Herasimau <yurik.by@gmail.com>
2021-10-22 04:13:07 +0300

    add cli.js

---
+++ bin/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const askName = () => {
+  const name = readlineSync.question('May I have your name? ');
+
+  return name;
+};
+
+export default askName;

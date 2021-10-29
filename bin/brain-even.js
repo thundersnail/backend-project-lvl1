d16d8aea66c728d3c41f596fc6d28445d7426406
Yury Herasimau <yurik.by@gmail.com>
2021-10-30 01:39:12 +0300

    add initial evengame implemetation

---
+++ bin/brain-even.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import { startEvengame } from '../src/braingames.js';
+
+startEvengame();
--- src/braingames.js
+++ src/braingames.js
@@ -28,3 +28,46 @@ export const getName = () => {
 export const getRandomInt = (maxInt) => Math.floor(Math.random() * maxInt);
 
 export const isEven = (num) => num % 2 === 0;
+
+export const startEvengame = () => {
+  const QUESTIONS = 3;
+  const RANGE = 30;
+  const M_Y = 'yes';
+  const M_N = 'no';
+  const M_RULES = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+  // greetings
+
+  greeting();
+  const name = getName();
+  say(M_RULES);
+
+  // start game
+  const M_WIN = `Congratulations, ${name}!`;
+
+  for (let q = 1; q <= QUESTIONS; q += 1) {
+    const number = getRandomInt(RANGE);
+    const correctAnswer = isEven(number) ? M_Y : M_N;
+
+    const M_QUESTION = `Question: ${number}`;
+    const M_ANSWER = 'Your answer: ';
+
+    say(M_QUESTION);
+    const userAnswer = interact(M_ANSWER);
+
+    // check answer
+    const M_CORRECT = 'Correct!';
+    const M_WRONG = `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`;
+
+    if (correctAnswer === userAnswer) {
+      say(M_CORRECT);
+    } else {
+      say(M_WRONG);
+      break;
+    }
+
+    if (q === QUESTIONS) {
+      say(M_WIN);
+    }
+  }
+};
